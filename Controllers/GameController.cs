afafe5194d79ca4a46e5fe57104706be5ac77f4b
Teresa Payne <teresa.payne@courts.in.gov>
2016-07-19 11:46:13 -0400

    test

---
+++ Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuessingGame.Models;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 10);
+
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess)
+        {
+            return guess == (int)Session["Answer"];
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(GameModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Win = GuessWasCorrect(model.Guess); 
+            }
+
+            return View(model);
+        }
+    }
+}
